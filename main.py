ad48dbd20f2523e0bab9e8d520be8fa75a67cc42
harisirgaming <123808950+harisirgaming@users.noreply.github.com>
2023-02-25 10:52:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ async def restart(client, m: Message):
     await asyncio.sleep(1)
     await accha.edit("𝐒𝐭𝐚𝐫𝐭𝐞𝐝..✓")
     await asyncio.sleep(0.2)
-    await accha.edit("🥵𝙽𝙾𝚆 𝙲𝙾𝙼𝙴 𝙷𝙴𝚁𝙴 👉 @TG_FRIENDSS🥀 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙶𝙸𝚁𝙻𝙵𝚁𝙸𝙴𝙽𝙳🌹")
+    await accha.edit("🥵𝙽𝙾𝚆 𝙲𝙾𝙼𝙴 𝙷𝙴𝚁𝙴 👉 @HARISIRYT🥀 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙶𝙸𝚁𝙻𝙵𝚁𝙸𝙴𝙽𝙳🌹")
          
 @bot.on_message(
     filters.command(["chatbot off", f"chatbot@{BOT_USERNAME} off"], prefixes=["/", ".", "?", "-"])
