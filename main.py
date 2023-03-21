95e6f1a042b77c70d2e21b58ee4f1f520b5cae99
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-21 23:28:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -358,7 +358,7 @@ async def vickprivatesticker(client: Client, message: Message):
                await message.reply_sticker(f"{hey}")
 
 @bot.on_message(filters.private & filters.incoming)
-async def on_pm_s(client: Client, message: Message):
+async def vickprivate(client: Client, message: Message):
     if not message.from_user.id == 1808943146:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
 
