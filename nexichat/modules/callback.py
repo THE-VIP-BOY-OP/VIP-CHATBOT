991348b1a827c050533ad9ba974afb9ffbb55648
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 08:53:59 +0530

    Add files via upload

---
+++ nexichat/modules/callback.py
@@ -0,0 +1,112 @@
+from pyrogram.enums import ChatMemberStatus as CMS
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
+
+from config import OWNER_USERNAME, SUPPORT_GRP
+
+from nexichat import nexichat
+from nexichat.database import DAXX
+from nexichat.modules.helpers import (
+    ABOUT_BTN,
+    ABOUT_READ,
+    ADMIN_READ,
+    BACK,
+    CHATBOT_BACK,
+    CHATBOT_READ,
+    DEV_OP,
+    HELP_BTN,
+    HELP_READ,
+    MUSIC_BACK_BTN,
+    SOURCE_READ,
+    START,
+    TOOLS_DATA_READ,
+)
+
+
+@nexichat.on_callback_query()
+async def cb_handler(_, query: CallbackQuery):
+    if query.data == "HELP":
+        await query.message.edit_text(
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+            disable_web_page_preview=True,
+        )
+    elif query.data == "CLOSE":
+        await query.message.delete()
+        await query.answer("ᴄʟᴏsᴇᴅ ᴍᴇɴᴜ!", show_alert=True)
+    elif query.data == "BACK":
+        await query.message.edit(
+            text=START,
+            reply_markup=InlineKeyboardMarkup(DEV_OP),
+        )
+    elif query.data == "SOURCE":
+        await query.message.edit(
+            text=SOURCE_READ,
+            reply_markup=InlineKeyboardMarkup(BACK),
+            disable_web_page_preview=True,
+        )
+    elif query.data == "ABOUT":
+        await query.message.edit(
+            text=ABOUT_READ,
+            reply_markup=InlineKeyboardMarkup(ABOUT_BTN),
+            disable_web_page_preview=True,
+        )
+    elif query.data == "ADMINS":
+        await query.message.edit(
+            text=ADMIN_READ,
+            reply_markup=InlineKeyboardMarkup(MUSIC_BACK_BTN),
+        )
+    elif query.data == "TOOLS_DATA":
+        await query.message.edit(
+            text=TOOLS_DATA_READ,
+            reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
+        )
+    elif query.data == "BACK_HELP":
+        await query.message.edit(
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+        )
+    elif query.data == "CHATBOT_CMD":
+        await query.message.edit(
+            text=CHATBOT_READ,
+            reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
+        )
+    elif query.data == "CHATBOT_BACK":
+        await query.message.edit(
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+        )
+    elif query.data == "addchat":
+        user_id = query.from_user.id
+        user_status = (await query.message.chat.get_member(user_id)).status
+        if user_status not in [CMS.OWNER, CMS.ADMINISTRATOR]:
+            return await query.answer(
+                "ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴇᴠᴇɴ ᴀɴ ᴀᴅᴍɪɴ, ᴅᴏɴ'ᴛ ᴛʀʏ ᴛʜɪs ᴇxᴘʟᴏsɪᴠᴇ sʜɪᴛ!",
+                show_alert=True,
+            )
+        else:
+            is_DAXX = DAXX.find_one({"chat_id": query.message.chat.id})
+            if not is_DAXX:
+                await query.edit_message_text(f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.**")
+            if is_DAXX:
+                DAXX.delete_one({"chat_id": query.message.chat.id})
+                await query.edit_message_text(
+                    f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}."
+                )
+    elif query.data == "rmchat":
+        user_id = query.from_user.id
+        user_status = (await query.message.chat.get_member(user_id)).status
+        if user_status not in [CMS.OWNER, CMS.ADMINISTRATOR]:
+            await query.answer(
+                "ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴇᴠᴇɴ ᴀɴ ᴀᴅᴍɪɴ, ᴅᴏɴ'ᴛ ᴛʀʏ ᴛʜɪs ᴇxᴘʟᴏsɪᴠᴇ sʜɪᴛ!",
+                show_alert=True,
+            )
+            return
+        else:
+            is_DAXX = DAXX.find_one({"chat_id": query.message.chat.id})
+            if not is_DAXX:
+                DAXX.insert_one({"chat_id": query.message.chat.id})
+                await query.edit_message_text(
+                    f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}."
+                )
+            if is_DAXX:
+                await query.edit_message_text("**ᴄʜᴀᴛ-ʙᴏᴛ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ.**")
---
+++ nexichat/modules/ping.py
@@ -0,0 +1,76 @@
+
+
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import OWNER_USERNAME
+from nexichat import nexichat
+from nexichat.database.chats import add_served_chat
+from nexichat.database.users import add_served_user
+from nexichat.modules.helpers import PNG_BTN
+
+
+#----------------IMG-------------#
+
+
+
+# Random Start Images
+IMG = [
+    "https://graph.org/file/210751796ff48991b86a3.jpg",
+    "https://graph.org/file/7b4924be4179f70abcf33.jpg",
+    "https://graph.org/file/f6d8e64246bddc26b4f66.jpg",
+    "https://graph.org/file/63d3ec1ca2c965d6ef210.jpg",
+    "https://graph.org/file/9f12dc2a668d40875deb5.jpg",
+    "https://graph.org/file/0f89cd8d55fd9bb5130e1.jpg",
+    "https://graph.org/file/e5eb7673737ada9679b47.jpg",
+    "https://graph.org/file/2e4dfe1fa5185c7ff1bfd.jpg",
+    "https://graph.org/file/36af423228372b8899f20.jpg",
+    "https://graph.org/file/c698fa9b221772c2a4f3a.jpg",
+    "https://graph.org/file/61b08f41855afd9bed0ab.jpg",
+    "https://graph.org/file/744b1a83aac76cb3779eb.jpg",
+    "https://graph.org/file/814cd9a25dd78480d0ce1.jpg",
+    "https://graph.org/file/e8b472bcfa6680f6c6a5d.jpg",
+]
+
+
+#----------------IMG-------------#
+
+#---------------STICKERS---------------#
+
+# Random Stickers
+STICKER = [
+    "CAACAgUAAx0CYlaJawABBy4vZaieO6T-Ayg3mD-JP-f0yxJngIkAAv0JAALVS_FWQY7kbQSaI-geBA",
+    "CAACAgUAAx0CYlaJawABBy4rZaid77Tf70SV_CfjmbMgdJyVD8sAApwLAALGXCFXmCx8ZC5nlfQeBA",
+    "CAACAgUAAx0CYlaJawABBy4jZaidvIXNPYnpAjNnKgzaHmh3cvoAAiwIAAIda2lVNdNI2QABHuVVHgQ",
+]
+
+#---------------STICKERS---------------#
+
+
+
+@nexichat.on_cmd("ping")
+async def ping(_, message: Message):
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{nexichat.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [MRDAXX❣️](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
