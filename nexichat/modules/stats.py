9e99fa031b5eabd739e60ce344e7abf8e7485d63
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 09:38:38 +0530

    Update stats.py

--- nexichat/modules/stats.py
+++ nexichat/modules/stats.py
@@ -4,14 +4,14 @@ from pyrogram.types import Message
 from nexichat import OWNER, nexichat
 from nexichat.database.chats import get_served_chats
 from nexichat.database.users import get_served_users
+from config import OWNER_ID
 
-
-@nexichat.on_message(filters.command("stats") & filters.user(OWNER))
+@nexichat.on_message(filters.command("stats") & filters.user(OWNER_ID))
 async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
     await message.reply_text(
-        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+        f"""{(await cli.get_me()).mention} ᴄʜᴀᴛʙᴏᴛ sᴛᴀᴛs:
 
 ➻ **ᴄʜᴀᴛs :** {chats}
 ➻ **ᴜsᴇʀs :** {users}"""
