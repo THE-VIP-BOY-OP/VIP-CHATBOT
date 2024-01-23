1eada76be192c3fbb1c21195d606f7777026c57d
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 08:34:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = getenv("OWNER_ID", "1808943146")
+SUPPORT_GRP = "TG_FRIENDSS"
+UPDATE_CHNL = "VIP_CREATORS"
+OWNER_USERNAME = "THE_VIP_BOY"
 
