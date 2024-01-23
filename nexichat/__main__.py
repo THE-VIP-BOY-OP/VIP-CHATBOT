140273afa0bcc9e272a699b461fef21d24ec1447
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-01-23 09:07:15 +0530

    Add files via upload

---
+++ nexichat/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from nexichat import LOGGER, nexichat
+from nexichat.modules import ALL_MODULES
+
+
+async def anony_boot():
+    try:
+        await nexichat.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("nexichat.modules." + all_module)
+
+    LOGGER.info(f"@{nexichat.username} Started.")
+    await idle()
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(anony_boot())
+    LOGGER.info("Stopping nexichat Bot...")
