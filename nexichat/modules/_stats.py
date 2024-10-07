f0e57f37a5a58f099482cc18458a36e505064858
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2024-10-07 23:05:40 +0530

    Update _stats.py

--- nexichat/modules/_stats.py
+++ nexichat/modules/_stats.py
@@ -1,18 +1,1 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
 
-from nexichat import nexichat
-from nexichat.database.chats import get_served_chats
-from nexichat.database.users import get_served_users
-
-
-@nexichat.on_message(filters.command("stats"))
-async def stats(cli: Client, message: Message):
-    users = len(await get_served_users())
-    chats = len(await get_served_chats())
-    await message.reply_text(
-        f"""{(await cli.get_me()).mention} ᴄʜᴀᴛʙᴏᴛ sᴛᴀᴛs:
-
-➻ **ᴄʜᴀᴛs :** {chats}
-➻ **ᴜsᴇʀs :** {users}"""
-    )
