b71560401fc4f9fdf9e71b768e7104ffe6f5929e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-07 16:31:52 +0530

    Update broadcast.py

--- nexichat/modules/broadcast.py
+++ nexichat/modules/broadcast.py
@@ -1,4 +1,10 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
 
+from nexichat import OWNER, nexichat
+from nexichat.database.chats import get_served_chats
+from nexichat.database.users import get_served_users
+from config import OWNER_ID
 
 
 import asyncio
