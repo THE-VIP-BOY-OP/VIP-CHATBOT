fd7c48eca08a86d0e24bb7cc228de400dc338a58
『 Krishna࿐』➙「✓」 <141105268+TG-LEGEND@users.noreply.github.com>
2024-10-07 23:06:22 +0530

    Update _start.py

--- nexichat/modules/_start.py
+++ nexichat/modules/_start.py
@@ -133,3 +133,147 @@ async def repo(_, m: Message):
 async def welcome(_, m: Message):
     for member in m.new_chat_members:
         await m.reply_photo(photo=random.choice(IMG), caption=START)
+
+
+import random
+from datetime import datetime
+
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import OWNER_USERNAME
+from nexichat import nexichat
+from nexichat.database.chats import add_served_chat
+from nexichat.database.users import add_served_user
+from nexichat.modules.helpers import PNG_BTN
+
+# ----------------IMG-------------#
+
+
+# Random Start Images
+IMG = [
+    "https://graph.org/file/210751796ff48991b86a3.jpg",
+    "https://graph.org/file/7b4924be4179f70abcf33.jpg",
+    "https://graph.org/file/f6d8e64246bddc26b4f66.jpg",
+    "https://graph.org/file/63d3ec1ca2c965d6ef210.jpg",
+    "https://graph.org/file/9f12dc2a668d40875deb5.jpg",
+    "https://graph.org/file/0f89cd8d55fd9bb5130e1.jpg",
+    "https://graph.org/file/e5eb7673737ada9679b47.jpg",
+    "https://graph.org/file/2e4dfe1fa5185c7ff1bfd.jpg",
+    "https://graph.org/file/36af423228372b8899f20.jpg",
+    "https://graph.org/file/c698fa9b221772c2a4f3a.jpg",
+    "https://graph.org/file/61b08f41855afd9bed0ab.jpg",
+    "https://graph.org/file/744b1a83aac76cb3779eb.jpg",
+    "https://graph.org/file/814cd9a25dd78480d0ce1.jpg",
+    "https://graph.org/file/e8b472bcfa6680f6c6a5d.jpg",
+]
+
+
+# ----------------IMG-------------#
+
+# ---------------STICKERS---------------#
+
+# Random Stickers
+STICKER = [
+    "CAACAgUAAx0CYlaJawABBy4vZaieO6T-Ayg3mD-JP-f0yxJngIkAAv0JAALVS_FWQY7kbQSaI-geBA",
+    "CAACAgUAAx0CYlaJawABBy4rZaid77Tf70SV_CfjmbMgdJyVD8sAApwLAALGXCFXmCx8ZC5nlfQeBA",
+    "CAACAgUAAx0CYlaJawABBy4jZaidvIXNPYnpAjNnKgzaHmh3cvoAAiwIAAIda2lVNdNI2QABHuVVHgQ",
+]
+
+# ---------------STICKERS---------------#
+
+
+@nexichat.on_cmd("ping")
+async def ping(_, message: Message):
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{nexichat.name} ᴄʜᴀᴛʙᴏᴛ ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴠɪᴘ ʙᴏʏ](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
+
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from nexichat import nexichat
+from nexichat.database.chats import get_served_chats
+from nexichat.database.users import get_served_users
+
+
+@nexichat.on_message(filters.command("stats"))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""{(await cli.get_me()).mention} ᴄʜᴀᴛʙᴏᴛ sᴛᴀᴛs:
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
+
+
+
+from pyrogram.enums import ParseMode
+
+from nexichat import nexichat
+
+
+@nexichat.on_cmd("id")
+async def getid(client, message):
+    chat = message.chat
+    your_id = message.from_user.id
+    message_id = message.id
+    reply = message.reply_to_message
+
+    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
+    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if not message.command:
+        message.command = message.text.split()
+
+    if len(message.command) == 2:
+        try:
+            split = message.text.split(None, 1)[1].strip()
+            user_id = (await client.get_users(split)).id
+            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+
+        except Exception:
+            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
+
+    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+
+    if (
+        not getattr(reply, "empty", True)
+        and not message.forward_from_chat
+        and not reply.sender_chat
+    ):
+        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
+        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+
+    if reply and reply.forward_from_chat:
+        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+        print(reply.forward_from_chat)
+
+    if reply and reply.sender_chat:
+        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
+        print(reply.sender_chat)
+
+    await message.reply_text(
+        text,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.DEFAULT,
+    )
+
