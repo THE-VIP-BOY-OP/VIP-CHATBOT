e928ccee520e78b2c8f95813abd8d52f08136ff2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-08 18:34:44 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -20,7 +20,7 @@ async def anony_boot():
 
     LOGGER.info(f"@{nexichat.username} Started.")
     try:
-        await nexichat.send_message(OWNER_ID, f"{nexichat.mention} has started")
+        await nexichat.send_message(int(OWNER_ID), f"{nexichat.mention} has started")
     except Exception as ex:
         LOGGER.info(f"@{nexichat.mention} Started please start the bot from owner id.")
     await idle()
