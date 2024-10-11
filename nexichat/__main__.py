5f3ff8d2916e71b1a24de466410fc98dceeb5959
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-11 22:04:08 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle
+from pyrogram.types import BotCommand
 from config import OWNER_ID
 from nexichat import LOGGER, nexichat
 from nexichat.modules import ALL_MODULES
@@ -18,14 +19,27 @@ async def anony_boot():
         importlib.import_module("nexichat.modules." + all_module)
         LOGGER.info(f"Successfully imported : {all_module}")
 
+    # Set bot commands
+    try:
+        await nexichat.set_bot_commands(
+            commands=[
+                BotCommand("start", "Start the bot"),
+                BotCommand("help", "Get the help menu"),
+                BotCommand("ping", "Check if the bot is alive or dead"),
+            ]
+        )
+        LOGGER.info("Bot commands set successfully.")
+    except Exception as ex:
+        LOGGER.error(f"Failed to set bot commands: {ex}")
+
     LOGGER.info(f"@{nexichat.username} Started.")
     try:
         await nexichat.send_message(int(OWNER_ID), f"{nexichat.mention} has started")
     except Exception as ex:
         LOGGER.info(f"@{nexichat.first_name} Started, please start the bot from owner id.")
+    
     await idle()
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("Stopping nexichat Bot...")
+    asyncio.get
